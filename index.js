036a8e7649b0397c4d0a4eaaa56517c5ae1802c1
awadesh <36awadesh@gmail.com>
2023-09-27 00:41:02 +0530

    changes on list items and Fixed Bug  of Error Regarding DOM.

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
+// Wait for the DOM (HTML content) to be fully loaded before executing any JavaScript code
+document.addEventListener("DOMContentLoaded", function () {
 
-let inputBtn = document.getElementById("input-btn");
+  const inputEl = document.getElementById("input-el");
+  const inputBtn = document.getElementById("input-btn");
+  const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click", function () {
-  console.log("Button clicked! by addEventListener");
+  let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+
+
+  inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value);
+  });
+
+
+  for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+
+    // Alternative way
+    /*
+     create element:- const li = document.createElement("li")
+    set text content:- li.textContent = myLeads[i]
+    append to ul :- ulEl.append(li)
+     */
+  }
+
 });
