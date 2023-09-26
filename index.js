8ca5eef71438fed85a7f434a10507f6af1a67c9d
awadesh <36awadesh@gmail.com>
2023-09-26 22:58:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function () {
+  console.log("Button clicked! by addEventListener");
+});
